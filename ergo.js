74f2a6edf045c39f3ad7788a87bafd6573487cb8
thorduragust <thordur.agust@gmail.com>
2020-06-07 23:50:08 +0000

    text

--- ergo.js
+++ ergo.js
@@ -32,6 +32,29 @@
 		context.lineWidth = prev_stroke_width;
 	}
 
+	function drawText(context, text, x, y, font, size, color) {
+		let prev_font = context.font;
+		let prev_color = context.fillStyle;
+
+		context.font = size.toString() + "px " + font;
+		context.fillStyle = color;
+		context.fillText(text, x, y);
+
+		context.font = prev_font;
+		context.fillStyle = prev_color;
+	}
+
+	function measureTextWidth(context, text, font, size) {
+		let result = 0.0;
+		let prev_font = context.font;
+
+		context.font = size.toString() + "px " + font;
+		result = context.measureText(text).width;
+		context.font = prev_font;
+
+		return result;
+	}
+
 	setInterval(function() {
 		context.canvas.width = window.innerWidth;
 		context.canvas.height = window.innerHeight;
@@ -47,5 +70,12 @@
 
 		drawLine(context, window.innerWidth/2.0, 0, window.innerWidth/2.0, window.innerHeight, "#0000FF", 5);
 		drawLine(context, 0, mouse_y, window.innerWidth, mouse_y, "#FF0000", 5);
+
+		let str = "hello";
+		let str_font = "serif";
+		let str_size = 120;
+		let text_width = measureTextWidth(context, str, str_font, str_size);
+
+		drawText(context, str, window.innerWidth/2.0 - text_width/2.0, mouse_y + str_size/4.0, str_font, str_size, "#000000");
 	}, 16);
 })();
